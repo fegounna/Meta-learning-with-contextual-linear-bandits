f29e5409dfcd01590e44cce206ab20743c18b92c
fegounna <yessin.moakher@polytechnique.edu>
2024-10-31 10:13:33 +0100

    meta learning algorithm

--- src/meta_learning.py
+++ src/meta_learning.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 
 np.random.seed(42)
 
-with open('../data/X_test.pkl', 'rb') as f:
+with open("../data/X_test.pkl", "rb") as f:
     X_test = pickle.load(f)
 
-with open('../data/Y_test.pkl', 'rb') as f:
+with open("../data/Y_test.pkl", "rb") as f:
     Y_test = pickle.load(f)
 
-B = np.load('../data/B_ml.npy')
+B = np.load("../data/B_ml.npy")
 
 # User t
 t = 4
 
-X_test_array = np.array(X_test[t]) 
-Y_test_array = np.array(Y_test[t])  
+X_test_array = np.array(X_test[t])
+Y_test_array = np.array(Y_test[t])
 
 n, d = X_test_array.shape
 
@@ -28,21 +28,21 @@ arms = set(range(n))
 arm = np.random.randint(0, n)
 arms.remove(arm)
 
-X_i = np.array([X_test_array[arm]])  
-Y_i = np.array([Y_test_array[arm]]) 
+X_i = np.array([X_test_array[arm]])
+Y_i = np.array([Y_test_array[arm]])
 
-for i in range(1, min(n//2,30)):
-    X_iTX_i = X_i.T @ X_i  
-    BTX_iTX_iB = B.T @ X_iTX_i @ B  
-    BTX_iTY_i = B.T @ X_i.T @ Y_i  
+for i in range(1, min(n // 2, 30)):
+    X_iTX_i = X_i.T @ X_i
+    BTX_iTX_iB = B.T @ X_iTX_i @ B
+    BTX_iTY_i = B.T @ X_i.T @ Y_i
 
     reg = 1e-6 * np.eye(BTX_iTX_iB.shape[0])
-    alpha_i = np.linalg.inv(BTX_iTX_iB + reg) @ BTX_iTY_i  
+    alpha_i = np.linalg.inv(BTX_iTX_iB + reg) @ BTX_iTY_i
 
     arm = max(arms, key=lambda a: np.dot(B @ alpha_i, X_test_array[a]))
 
-    X_i = np.vstack([X_i, X_test_array[arm]]) 
-    Y_i = np.append(Y_i, Y_test_array[arm])  
+    X_i = np.vstack([X_i, X_test_array[arm]])
+    Y_i = np.append(Y_i, Y_test_array[arm])
 
     arms.remove(arm)
 
@@ -53,30 +53,30 @@ arms = set(range(n))
 arm = np.random.randint(0, n)
 arms.remove(arm)
 
-Y_i_random = np.array([Y_test_array[arm]]) 
+Y_i_random = np.array([Y_test_array[arm]])
 
-for i in range(1,min(n//2,30)):
+for i in range(1, min(n // 2, 30)):
     arm = np.random.choice(list(arms))
     arms.remove(arm)
     Y_i_random = np.append(Y_i_random, Y_test_array[arm])
 
 
-
 fig, axs = plt.subplots(2, 1, figsize=(10, 12))
 
-axs[0].plot(Y_i, marker='o', linestyle='', color='b')
+axs[0].plot(Y_i, marker="o", linestyle="", color="b")
 axs[0].set_title(f"Reward of Y_{t} with the meta-represented greedy policy")
 axs[0].set_xlabel("Iteration")
 axs[0].set_ylabel("Rating")
+axs[0].set_ylim(0.9, 5.1)
 axs[0].grid(True)
 
-axs[1].plot(Y_i_random, marker='o', linestyle='', color='b')
+axs[1].plot(Y_i_random, marker="o", linestyle="", color="b")
 axs[1].set_title(f"Reward of Y_{t} with the random selection of the arms")
 axs[1].set_xlabel("Iteration")
 axs[1].set_ylabel("Rating")
+axs[1].set_ylim(0.9, 5.1)
 axs[1].grid(True)
 
-plt.savefig(f'../results/user{t}')
+plt.savefig(f"../results/user{t}")
 plt.tight_layout()
 plt.show()
-
--- src/trace_norm_regularization.py
+++ src/trace_norm_regularization.py
@@ -2,9 +2,10 @@
 
 import numpy as np
 
+
 def compute_gradient(A, X, Y):
     """Compute the gradient of the our function f.
-    
+
     Parameters
     ----------
     A : array-like, shape (d, T)
@@ -13,7 +14,7 @@ def compute_gradient(A, X, Y):
         The context data.
     Y : array-like, shape (T, n)
         The reward data.
-    
+
     Returns
     -------
     grad : array-like, shape (d, T)
@@ -25,9 +26,10 @@ def compute_gradient(A, X, Y):
     grad = np.zeros_like(A)
 
     for t in range(T):
-        grad[:,t] += X[t].T @(Y[t].T- X[t]@A[:,t])
-    grad *= -2/(T*n)
+        grad[:, t] += X[t].T @ (Y[t].T - X[t] @ A[:, t])
+    grad *= -2 / (T * n)
     return grad
+
 
 def singular_value_thresholding(A, λ):
     """Apply the singular value thresholding operator.
@@ -38,7 +40,7 @@ def singular_value_thresholding(A, λ):
         The matrix to threshold.
     λ : float
         The threshold.
-    
+
     Returns
     -------
     A_thresh : array-like, shape (d, T)
@@ -49,11 +51,9 @@ def singular_value_thresholding(A, λ):
     return u @ np.diag(s) @ vh
 
 
-
-
-def update_rule(W,X,Y,L,λ):
+def update_rule(W, X, Y, L, λ):
     """Update the matrix W using the proximal gradient descent.
-    
+
     Parameters
     ----------
     W : array-like, shape (d, T)
@@ -66,25 +66,26 @@ def update_rule(W,X,Y,L,λ):
         The Lipschitz constant of the gradient of f.
     λ : float
         The regularization parameter.
-    
+
     Returns
     -------
     W_new : array-like, shape (d, T)
         The updated matrix.
     """
 
-    C = W - 1/L * compute_gradient(W,X,Y)
-    return singular_value_thresholding(C,λ/L)
-    
+    C = W - 1 / L * compute_gradient(W, X, Y)
+    return singular_value_thresholding(C, λ / L)
+
+
 def calculate_lipschitz_constant(X):
     """
     Calculate the Lipschitz constant L for the gradient of f.
-    
+
     Parameters
     ----------
     X : array-like, shape (T, n, d)
         The context data.
-    
+
     Returns
     -------
     L : float
@@ -96,13 +97,12 @@ def calculate_lipschitz_constant(X):
         # Compute the largest singular value of X_t
         singular_values = np.linalg.svd(X[t], compute_uv=False)
         sigma_max = np.max(singular_values)
-        max_singular_value_squared = max(max_singular_value_squared, sigma_max ** 2)
+        max_singular_value_squared = max(max_singular_value_squared, sigma_max**2)
     L = (2 / (n * T)) * max_singular_value_squared
     return L
 
 
 def optimize_under_trace_norm_regularization(A0, X, Y, λ=0.1, n_iter=100):
-    
     """Compute an estimate using Accelerated Gradient Method for Trace Norm Minimization [Ji, 2009] of the problem : min f(A) + λ||A||_*
 
     Parameters
@@ -126,25 +126,25 @@ def optimize_under_trace_norm_regularization(A0, X, Y, λ=0.1, n_iter=100):
 
     alpha_k = 1
     Z_k = A0
-    W_k = A0 
+    W_k = A0
     L = calculate_lipschitz_constant(X)
     print(L)
     for _ in range(n_iter):
+        W_k_next = update_rule(Z_k, X, Y, L, λ)
 
-        W_k_next = update_rule(Z_k,X,Y,L,λ)
+        alpha_k_next = (1 + np.sqrt(1 + 4 * alpha_k**2)) / 2
 
-        alpha_k_next = (1+np.sqrt(1+4*alpha_k**2))/2
-
-        Z_k = W_k + (alpha_k - 1)/alpha_k_next * (W_k_next - W_k)
+        Z_k = W_k + (alpha_k - 1) / alpha_k_next * (W_k_next - W_k)
         alpha_k = alpha_k_next
         W_k = W_k_next
-    
+
     return W_k
+
 
 def extract_left_singular_vectors(A, threshold=0.01):
     """Extract the left singular vectors of a matrix using Singular Value Decomposition (SVD),
     retaining only those corresponding to singular values greater than a specified threshold.
-    
+
     Parameters
     ----------
     A : array-like, shape (d, T)
@@ -155,19 +155,20 @@ def extract_left_singular_vectors(A, threshold=0.01):
     Returns
     -------
     B : array-like, shape (d, r)
-        The matrix of left singular vectors of A, where r is the number of singular values 
+        The matrix of left singular vectors of A, where r is the number of singular values
         greater than the threshold.
     """
     U, S, _ = np.linalg.svd(A, full_matrices=False)
-    
+
     r = np.sum(S > threshold)
-    
-    B = U[:, :r]        # Shape: (d, r)
+
+    B = U[:, :r]  # Shape: (d, r)
 
     return B
 
-X = np.load('../data/X_ml.npy')
-Y = np.load('../data/Y_ml.npy')
+
+X = np.load("../data/X_ml.npy")
+Y = np.load("../data/Y_ml.npy")
 A0 = np.random.rand(X.shape[2], X.shape[0])
 λ = 0.005
 n_iter = 100
@@ -176,5 +177,4 @@ A = optimize_under_trace_norm_regularization(A0, X, Y, λ, n_iter)
 
 B = extract_left_singular_vectors(A, threshold=0.01)
 
-np.save('../data/B_ml.npy', B)
-
+np.save("../data/B_ml.npy", B)
