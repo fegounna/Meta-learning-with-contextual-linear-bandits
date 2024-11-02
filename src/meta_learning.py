673ee9121be12e4b0a1ea77498fbfbf82eccdf5e
fegounna <yessin.moakher@polytechnique.edu>
2024-11-02 19:35:14 +0100

    add method of moments

--- src/meta_learning.py
+++ src/meta_learning.py
@@ -12,46 +12,55 @@ with open("../data/X_test.pkl", "rb") as f:
 with open("../data/Y_test.pkl", "rb") as f:
     Y_test = pickle.load(f)
 
-B = np.load("../data/B_ml.npy")
+B_trace = np.load("../data/B_ml.npy")
+B_mom = np.load("../data/B_ml_mom.npy")
 
 total_iterations = 20
 
-# Algorithm : Meta-Represented Greedy Policy
-regrets = np.zeros(total_iterations)
 
-for t in range(5):
-    X_test_array = np.array(X_test[t])
-    Y_test_array = np.array(Y_test[t])
+def calculate_regret(X_test, Y_test, B):
+    regrets = np.zeros(total_iterations)
 
-    n, d = X_test_array.shape
+    for t in range(5):
+        X_test_array = np.array(X_test[t])
+        Y_test_array = np.array(Y_test[t])
 
-    arms = set(range(n))
-    arm = np.random.randint(0, n)
-    arms.remove(arm)
+        n, d = X_test_array.shape
 
-    X_i = np.array([X_test_array[arm]])
-    Y_i = np.array([Y_test_array[arm]])
-
-    best_reward = np.max(Y_test_array)
-    regrets[0] += best_reward - Y_test_array[arm]
-
-    for i in range(1, total_iterations):
-        X_iTX_i = X_i.T @ X_i
-        BTX_iTX_iB = B.T @ X_iTX_i @ B
-        BTX_iTY_i = B.T @ X_i.T @ Y_i
-
-        reg = 1e-6 * np.eye(BTX_iTX_iB.shape[0])
-        alpha_i = np.linalg.inv(BTX_iTX_iB + reg) @ BTX_iTY_i
-
-        arm = max(arms, key=lambda a: np.dot(B @ alpha_i, X_test_array[a]))
-        best_reward = np.max([Y_test_array[a] for a in arms])
-
-        X_i = np.vstack([X_i, X_test_array[arm]])
-        Y_i = np.append(Y_i, Y_test_array[arm])
-
-        regrets[i] += best_reward - Y_test_array[arm]
+        arms = set(range(n))
+        arm = np.random.randint(0, n)
         arms.remove(arm)
 
+        X_i = np.array([X_test_array[arm]])
+        Y_i = np.array([Y_test_array[arm]])
+
+        best_reward = np.max(Y_test_array)
+        regrets[0] += best_reward - Y_test_array[arm]
+
+        for i in range(1, total_iterations):
+            X_iTX_i = X_i.T @ X_i
+            BTX_iTX_iB = B.T @ X_iTX_i @ B
+            BTX_iTY_i = B.T @ X_i.T @ Y_i
+
+            reg = 1e-6 * np.eye(BTX_iTX_iB.shape[0])
+            alpha_i = np.linalg.inv(BTX_iTX_iB + reg) @ BTX_iTY_i
+
+            arm = max(arms, key=lambda a: np.dot(B @ alpha_i, X_test_array[a]))
+            best_reward = np.max([Y_test_array[a] for a in arms])
+
+            X_i = np.vstack([X_i, X_test_array[arm]])
+            Y_i = np.append(Y_i, Y_test_array[arm])
+
+            regrets[i] += best_reward - Y_test_array[arm]
+            arms.remove(arm)
+    return regrets
+
+
+# Algorithm : Meta-Represented Greedy Policy
+regrets_trace = calculate_regret(X_test, Y_test, B_trace)
+
+# Algorithm : Method of Moments
+regrets_mom = calculate_regret(X_test, Y_test, B_mom)
 
 # Algorithm : Random Selection of the arms
 regrets_random = np.zeros(total_iterations)
@@ -70,16 +79,21 @@ for t in range(5):
         regrets_random[i] += best_reward - Y_test_array[arm]
 
 
-cumulative_regrets = np.cumsum(regrets)
+cumulative_regrets_trace = np.cumsum(regrets_trace)
 cumulative_regrets_random = np.cumsum(regrets_random)
+cumulative_regrets_mom = np.cumsum(regrets_mom)
 
 
 plt.figure(figsize=(12, 6))
-plt.plot(cumulative_regrets / total_iterations, label="Meta-represented greedy policy")
+plt.plot(
+    cumulative_regrets_trace / total_iterations,
+    label="Trace norm regularization Method",
+)
+plt.plot(cumulative_regrets_mom / total_iterations, label="Method of Moments")
 plt.plot(cumulative_regrets_random / total_iterations, label="Random selection")
 plt.xlabel("Iteration")
 plt.ylabel("Average Cumulative Regret")
-plt.title("Comparison of the Meta-represented greedy policy with random selection")
+plt.title("Comparison of the Meta-represented greedy policies with random selection")
 plt.legend()
 plt.grid(True)
 plt.savefig("../results/Average Cumulative Regret of 5 test users.png")
---
+++ src/method_of_moments.py
@@ -0,0 +1,45 @@
+"""Implementation of method of moments [Nilesh Tripuraneni, Chi Jin, and Michael Jordan, 2021] Provable meta-learning of linear representations"""
+
+import numpy as np
+
+
+def estimate_B_using_mom(X, Y, r):
+    """
+    Estimate the matrix B using the method of moments.
+
+    Parameters
+    ----------
+    X : array-like, shape (T, n, d)
+        The context data.
+    Y : array-like, shape (T, n)
+        The reward data.
+    r : int
+        The number of singular values to retain.
+
+    Returns
+    -------
+    B : array-like, shape (d, r)
+        The estimation of the matrix B.
+    """
+    T, n, d = X.shape
+
+    result_matrix = np.zeros((d, d))
+
+    for t in range(T):
+        for i in range(n):
+            y_squared = Y[t, i] ** 2
+            x_i = X[t, i].reshape(d, 1)
+            result_matrix += y_squared * (x_i @ x_i.T)
+
+    result_matrix /= n * T
+
+    U, _, _ = np.linalg.svd(result_matrix, full_matrices=False)
+    B = U[:, :r]
+    return B
+
+
+X = np.load("../data/X_ml.npy")
+Y = np.load("../data/Y_ml.npy")
+
+B = estimate_B_using_mom(X, Y, 3)
+np.save("../data/B_ml_mom.npy", B)
